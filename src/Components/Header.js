70aed7a8f0ac8cc1428f7005f0adbda81488cb08
jztimm <jztimm@gmail.com>
2021-03-31 23:37:17 -0400

    Removed Font Awesome and replaced with Material UI

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Navbar, Nav, Container} from 'react-bootstrap'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const Header = () => {
   return (
     // Navbar bg not working properly (color) / Standard Navbar setup w/o dropdowns and search
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <Navbar.Brand href="/">Merch Store</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">Cart</Nav.Link>
-              <Nav.Link href="/login">Sign In</Nav.Link>
+              <Nav.Link href="/cart"><ShoppingCartIcon/> Cart</Nav.Link>
+              <Nav.Link href="/login"><AccountCircleIcon/> Sign In</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
